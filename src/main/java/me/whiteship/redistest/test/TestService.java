3e48b4386dbf88214982f9ccbd2135e8c2e934a7
KrongDev <138358867+KrongDev@users.noreply.github.com>
2024-03-15 16:50:24 +0900

    Update TestService.java

--- src/main/java/me/whiteship/redistest/test/TestService.java
+++ src/main/java/me/whiteship/redistest/test/TestService.java
@@ -1,2 +1,34 @@
-package me.whiteship.redistest.test;public class TestService {
+package me.whiteship.redistest.test;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class TestService {
+    //
+    private final RedisTemplate<String, String> redisTemplate;
+
+    public void create(String key, String data) {
+        ValueOperations<String, String> opreation = redisTemplate.opsForValue();
+        opreation.set(key, data);
+    }
+
+    public void update(String key, String data) {
+        ValueOperations<String, String> opreation = redisTemplate.opsForValue();
+        opreation.set(key, data);
+    }
+
+    public String load(String key) {
+        ValueOperations<String, String> opreation = redisTemplate.opsForValue();
+        return opreation.get(key);
+    }
+
+    public void remove(String key) {
+        redisTemplate.delete(key);
+    }
 }
